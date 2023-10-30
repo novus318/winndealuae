3f2f86f38b6294e2539f8ba930c74e62cb14ed96
novus318 <nizamudheen318@gmail.com>
2023-10-30 15:54:51 +0530

    style err

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { useState, useEffect } from 'react';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const [tailwindLoaded, setTailwindLoaded] = useState(false);
-
-  useEffect(() => {
-    const link = document.createElement('link');
-    link.href = 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css';
-    link.rel = 'stylesheet';
-    link.onload = () => {
-      setTailwindLoaded(true);
-    };
-    document.head.appendChild(link);
-  }, []);
-
   return (
     <Html lang="en">
-      <Head>
-        <link
+      <Head />
+      <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="/title.webp"
         />
-      </Head>
+        <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+/>
+
       <body>
-        {tailwindLoaded ? (
-          <Main />
-        ) : null}
+        <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
