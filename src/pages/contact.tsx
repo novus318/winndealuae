936dcc6c83fc7e769910e146212995a5ea9a2b1d
novus318 <nizamudheen318@gmail.com>
2023-11-19 11:42:20 +0530

    contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,6 +6,8 @@ import contact from '@/images/conta.webp';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import Layout from '@/Components/Layout';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -17,7 +19,7 @@ const Contact = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    subject: '',
+    number: '',
     message: '',
   });
 
@@ -78,55 +80,55 @@ const Contact = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
 
     const form = document.createElement('form');
-  
-  
-    form.method = 'post'; 
-    form.action = ''; 
+
+
+    form.method = 'post';
+    form.action = '';
     const nameField = document.createElement('input');
     nameField.type = 'text';
     nameField.name = 'name';
     nameField.value = formData.name;
     form.appendChild(nameField);
-  
+
     const emailField = document.createElement('input');
     emailField.type = 'text';
     emailField.name = 'email';
     emailField.value = formData.email;
     form.appendChild(emailField);
-  
-    const subjectField = document.createElement('input');
-    subjectField.type = 'text';
-    subjectField.name = 'subject';
-    subjectField.value = formData.subject;
-    form.appendChild(subjectField);
-  
+
+    const numberField = document.createElement('input');
+    numberField.type = 'text';
+    numberField.name = 'number';
+    numberField.value = formData.number;
+    form.appendChild(numberField);
+
     const messageField = document.createElement('textarea');
     messageField.name = 'message';
     messageField.value = formData.message;
     form.appendChild(messageField);
-  
+
     document.body.appendChild(form);
-  
+
     setFormData({
       name: '',
-    email: '',
-    subject: '',
-    message: '',
+      email: '',
+      number: '',
+      message: '',
     })
     emailjs.sendForm(SERVICE_ID_EMAIL, EMAILJS_TEMPLATE_ID, form, EMAIL_USER_ID)
       .then((result) => {
         console.log('Email sent successfully!', result.text);
-        
+
       }, (error) => {
         console.error('Email could not be sent:', error);
       });
-  
+
     document.body.removeChild(form);
   };
-  
+
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -134,82 +136,83 @@ const Contact = () => {
 
   return (
     <Layout
-    title="WinnDeal Solutions - Ask for a Quote"
-    description="We deliver cutting-edge IT solutions tailored to your business needs in UAE, Our team of experts is committed to providing top-notch services that drive results"
-    keywords="Computer shop dubai, Online laptop uae, Computer shop in dubai, Buy laptop online in uae, Laptop accessories in dubai, Computer accessories in uae, Buy laptop online dubai, Laptop accessories dubai,Ask for a Quote"
-    author="Muhammed Nizamudheen M"
-    canonicalUrl="https://winndeal.com/contact">
-    <>
-      <Header />
-      <section className="py-10 bg-gray-100 text-[#282828] px-5 lg:px-20 mt-16">
-        <div className="flex flex-col lg:flex-row items-center">
-          <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
-            <img src={contact.src} alt="Illustration" className="w-full h-auto contact-image" />
-          </div>
+      title="WinnDeal Solutions - Ask for a Quote"
+      description="We deliver cutting-edge IT solutions tailored to your business needs in UAE, Our team of experts is committed to providing top-notch services that drive results"
+      keywords="Computer shop dubai, Online laptop uae, Computer shop in dubai, Buy laptop online in uae, Laptop accessories in dubai, Computer accessories in uae, Buy laptop online dubai, Laptop accessories dubai,Ask for a Quote"
+      author="Muhammed Nizamudheen M"
+      canonicalUrl="https://winndeal.com/contact">
+      <>
+        <Header />
+        <section className="py-10 bg-gray-100 text-[#282828] px-5 lg:px-20 mt-16">
+          <div className="flex flex-col lg:flex-row items-center">
+            <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+              <img src={contact.src} alt="Illustration" className="w-full h-auto contact-image" />
+            </div>
 
-          <div className="w-full lg:w-1/2">
-            <h2 className="text-2xl lg:text-4xl font-bold mb-6 text-center">Contact Us</h2>
-            <form onSubmit={handleSubmit} className="bg-[#282828] p-4 form-element lg:p-6 rounded-lg shadow-lg">
-              <div className="mb-6 mt-2">
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  className="w-full border rounded-lg py-2 px-3"
-                />
-              </div>
-              <div className="mb-6">
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className="w-full border rounded-lg py-2 px-3"
-                />
-              </div>
-              <div className="mb-6">
-                <input
-                  type="text"
-                  id="subject"
-                  name="subject"
-                  placeholder="Subject"
-                  value={formData.subject}
-                  onChange={handleChange}
-                  className="w-full border rounded-lg py-2 px-3"
-                />
-              </div>
-              <div className="mb-6">
-                <textarea
-                  id="message"
-                  name="message"
-                  placeholder="Message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  className="w-full border rounded-lg py-2 px-3"
-                />
-              </div>
-              <button type="submit" className="bg-[#ffdd00df] text-white py-3 font-semibold px-4 rounded-lg w-full my-2">
-                Send Message
-              </button>
-            </form>
+            <div className="w-full lg:w-1/2">
+              <h2 className="text-2xl lg:text-4xl font-bold mb-6 text-center">Contact Us</h2>
+              <form onSubmit={handleSubmit} className="bg-[#282828] p-4 form-element lg:p-6 rounded-lg shadow-lg">
+                <div className="mb-6 mt-2">
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="w-full border rounded-lg py-2 px-3  bg-white"
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full border rounded-lg py-2 px-3  bg-white"
+                  />
+                </div>
+                <div className="mb-6">
+                  <PhoneInput
+                    international
+                    defaultCountry="AE"
+                    maxLength={16}
+                    required
+                    placeholder="Phone Number"
+                    value={formData.number}
+                    onChange={(value: any) => setFormData({ ...formData, number: value })}
+                    className="w-full border rounded-lg py-2 px-3 bg-white"
+                  />
+                </div>
+                <div className="mb-6">
+                  <textarea
+                    id="message"
+                    name="message"
+                    placeholder="Message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    className="w-full border rounded-lg py-2 px-3  bg-white"
+                  />
+                </div>
+                <button type="submit" className="bg-[#ffdd00df] text-white py-3 font-semibold px-4 rounded-lg w-full my-2">
+                  Send Message
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="bg-gray-100 text-[#282828] py-6 px-5 lg:px-20">
-        <div className="container conte mx-auto">
-          <h2 className="text-xl lg:text-2xl font-bold mb-3 text-center">We would Love to Hear from You</h2>
-          <p className="text-sm lg:text-base text-center lg:text-center text-gray-600">
-            Feel free to reach out to us for any inquiries or feedback. Our team is here to assist you.
-          </p>
-        </div>
-      </section>
-      <Footer />
-    </>
+        </section>
+        <section className="bg-gray-100 text-[#282828] py-6 px-5 lg:px-20">
+          <div className="container conte mx-auto">
+            <h2 className="text-xl lg:text-2xl font-bold mb-3 text-center">We would Love to Hear from You</h2>
+            <p className="text-sm lg:text-base text-center lg:text-center text-gray-600">
+              Feel free to reach out to us for any inquiries or feedback. Our team is here to assist you.
+            </p>
+          </div>
+        </section>
+        <Footer />
+      </>
     </Layout>
   );
 };
