8b0e188716b9194b36ec66bb59c4038d332f1e65
novus318 <nizamudheen318@gmail.com>
2024-01-19 16:52:13 +0400

    preview image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,11 @@ export default function Document() {
               __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${gtag.GA_TRACKING_ID}');`,
             }}
           />
+          <meta property="og:title" content="WinnDeal IT Solutions and Web development" />
+        <meta property="og:description" content="We deliver cutting-edge IT solutions and Web Developments tailored to your business needs in UAE, Our team of experts is committed to providing top-notch services that drive results" />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:url" content="https://www.winndeal.com/" />
+        <meta name="twitter:card" content="/ogimage.png" />
       </Head>
       <link
           rel="icon"
