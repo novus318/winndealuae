d8f91fb21dede49bdadaabc35f606843b8637530
Bob <winndealuae@gmail.com>
2024-06-01 13:18:05 +0400

    clear

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { FaCheckCircle, FaHandshake, FaUsers } from 'react-icons/fa';
 import Layout from '../components/Layout';
+import Footer from '@/components/Footer';
 
 
 gsap.registerPlugin(ScrollTrigger);
