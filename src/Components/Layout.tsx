828e328979857d1e2fa569d02acf9d076f01ed53
Bob <winndealuae@gmail.com>
2024-06-01 13:09:38 +0400

    errror

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import ScrollUp from '@/components/ScrollUp'
+import ScrollUp from '../components/ScrollUp'
 import ContactIcons from './ContactIcons';
 interface LayoutProps {
   children: React.ReactNode;
